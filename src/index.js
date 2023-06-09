fb614155cbf3771de516e8564cf046a56b6de61f
MadebyMarco <marco3xmedina@gmail.com>
2023-06-08 18:53:48 -0700

    add event listeners

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import { formatDistance, formatDistanceToNow, subDays } from "date-fns";
 import { DOM } from "./dom";
+import { addEventListeners } from "./ui";
 // const project = (title, items = []) => {
 //     return {title, items}
 // }
 
 
 DOM.load();
+addEventListeners();
+
 // todo: separate dom and logic even further
 // todo: use date-fns for a date created :)

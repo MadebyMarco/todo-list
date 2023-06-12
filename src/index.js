08a70d61a4090cdb2688ad4cfd122b4df3ce390c
MadebyMarco <marco3xmedina@gmail.com>
2023-06-12 11:39:19 -0700

    error handling: sets storage on first time load

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import { formatDistance, formatDistanceToNow, subDays } from "date-fns";
 import { DOM } from "./dom";
 import { addEventListeners } from "./ui";
+import { getProjectsContainerFromStorage, setProjectsContainerFromStorage, syncProjectsContainers } from "./logic";
 // const project = (title, items = []) => {
 //     return {title, items}
 // }
 
+//sets local storage for the first time
+if(getProjectsContainerFromStorage() == null) {
+    setProjectsContainerFromStorage();
+}
+//updates local project container for storage
+syncProjectsContainers();
 
 DOM.load();
 addEventListeners();

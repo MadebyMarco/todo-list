ac8ede7e3acaba793232540d30d25e81110c4a1a
MadebyMarco <marco3xmedina@gmail.com>
2023-06-15 13:31:24 -0700

    remove useless code

--- src/index.js
+++ src/index.js
@@ -1,26 +1,13 @@
 import { formatDistance, formatDistanceToNow, subDays } from "date-fns";
 import { DOM } from "./dom";
-import { addEventListenerToBody, addEventListeners } from "./ui";
+import { addEventListeners } from "./ui";
 import {
   getProjectsContainerFromStorage,
   setProjectsContainerFromStorage,
   syncProjectsContainers,
 } from "./logic";
-// const project = (title, items = []) => {
-//     return {title, items}
-// }
-
-//sets local storage for the first time
-// if (getProjectsContainerFromStorage() == null) {
-//   console.log("Initializing for the first time");
-//   setProjectsContainerFromStorage();
-// }
+if (getProjectsContainerFromStorage() == null)
+  setProjectsContainerFromStorage();
 syncProjectsContainers();
-//updates local project container for storage
-
 DOM.load();
 addEventListeners();
-addEventListenerToBody();
-
-// todo: separate dom and logic even further
-// todo: use date-fns for a date created :)

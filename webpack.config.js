b0fc7e25b9b9a5f9a184eeac60d930838b2123cd
MadebyMarco <marco3xmedina@gmail.com>
2023-06-16 14:24:36 -0700

    add css

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,23 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  mode: 'development',
+  mode: "development",
   entry: {
-    index: './src/index.js',
-    dom: './src/dom.js',
-    logic: './src/logic.js',
-    ui: './src/ui.js',
+    index: "./src/index.js",
+    dom: "./src/dom.js",
+    logic: "./src/logic.js",
+    ui: "./src/ui.js",
   },
   output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
   },
 };
